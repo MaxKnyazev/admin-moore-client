f3d9b1330d09e4207c5f4c9ed7beea201400d814
MaxKnyazev <max.knyazev77@gmail.com>
2023-05-06 11:12:27 +0300

    2023-05-06 add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store'
+import App from './components/App';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-      <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </BrowserRouter>
 );
