756ea2bf58225b02845449dcaf7c6c568ed09912
MaxKnyazev <max.knyazev77@gmail.com>
2023-05-06 12:12:39 +0300

    2023-05-06 add pages NotFoundPage, HomePage and add components HomePage, NotFound, Layout

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import './NotFound.scss';
+
+function NotFound() {
+  return (
+    <div className='notfound'>
+      <h1 className='notfound__text'>Страница не найдена 404</h1>
+      <span className='notfound__smile'>:с</span>
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import './NotFoundPage.scss';
+import NotFound from '../../components/NotFound';
+
+function NotFoundPage() {
+  return (
+    <section className='notfoundpage'>
+      <NotFound />
+    </section>
+  );
+}
+
+export default NotFoundPage;
