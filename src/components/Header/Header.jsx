37a9c4dceecb3731b88eb2eb3c444b4f0fe5fe41
MaxKnyazev <max.knyazev77@gmail.com>
2023-05-13 18:13:03 +0300

    2023-05-13 display count guests

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,23 @@ import { useSelector } from 'react-redux';
 
 function Header() {
   const { cashbox } = useSelector(state => state.shiftsReducer);
+  const { guests } = useSelector(state => state.guestsReducer);
+
+  let activeGuests = 0;
+  for (const guest of guests) {
+    if (guest.result_money === null) {
+      activeGuests += 1;
+    }
+  }
+
   return (
       <header className="header">
         <h1 className="header__title">Admin Moore</h1>
-        <div className="header__cashbox">Cashbox: {cashbox}</div>
+        <div className="header__span">Cashbox: {cashbox}</div>
+        <div className="header__guests">
+          <div className="header__span">Гостей в зале: {activeGuests}</div>
+          <div className="header__span">Гостей прошло за день: {guests.length}</div>
+        </div>
       </header>
   )
 }
