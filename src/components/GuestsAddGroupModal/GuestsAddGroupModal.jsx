79c4f2a6e252edbad90e6946eed55e4787c88ede
MaxKnyazev <max.knyazev77@gmail.com>
2023-05-20 20:15:27 +0300

    2023-05-20 update guestsAddGroupModal

--- src/components/GuestsAddGroupModal/GuestsAddGroupModal.jsx
+++ src/components/GuestsAddGroupModal/GuestsAddGroupModal.jsx
@@ -5,16 +5,8 @@ import { editGuestAsync, toggleShowAddGroupModal } from '../../store/guests/gues
 
 function GuestsAddGroupModal() {
   const dispatch = useDispatch();
-  const [ group, setGroup ] = useState([
-    { name: 'guest', id: '123' },
-    { name: 'guest', id: '123' },
-    { name: 'guest', id: '123' },
-    { name: 'guest', id: '123' },
-    { name: 'guest', id: '123' },
-    { name: 'guest', id: '123' },
-    { name: 'guest', id: '123' },
-    { name: 'guest', id: '123' },
-  ]);
+  const [ inputGroupName, setInputGroupName ] = useState('');
+  const [ group, setGroup ] = useState([]);
 
   const inputHandler = (e, setInput) => {
     setInput(e.target.value);
@@ -24,27 +16,41 @@ function GuestsAddGroupModal() {
     dispatch(toggleShowAddGroupModal());
   }
 
+  const addToGroupButtonHandler = () => {
+    setGroup([...group, {
+      tariff: '1',
+      id: Date.now()
+    }])
+  }
+
+  const deleteGuestButtonHandler = (id) => {
+    setGroup(group.filter(elem => elem.id !== id))
+  }
+
+  let count = 0;
+
   return (
     <section className="addGroupModal">
       <div className="addGroupModal__wrapper">
         <div className="addGroupModal__form form">
           <span className="form__title">Название группы:</span>
-          <input className="form__input" placeholder="Group name..." type="text" value={() => {}} onChange={() => {}} />
-          <button className="form__button">Добавить человека</button>
+          <input className="form__input" placeholder="Group name..." type="text" value={inputGroupName} onChange={e => {inputHandler(e, setInputGroupName)}} />
+          <button className="form__button" onClick={addToGroupButtonHandler}>Добавить человека</button>
         </div>
 
         <ul className="addGroupModal__list">
           {group.map(elem => {
+            count += 1;
             return (
               <li key={elem.id} className="addGroupModal__item item">
-                <select defaultValue="1" onChange={() => {}} className="item__tariff" name="tariff">
+                <select className="item__tariff" defaultValue={elem.tariff} onChange={() => {}} name="tariff">
                   <option value="1">Взрослый</option>
                   <option value="2">Детский</option>
                 </select>
 
-                <span className="item__titel">+1</span>
+                <span className="item__titel">+{count}</span>
 
-                <button className="item__button">Удалить</button>
+                <button className="item__button" onClick={() => {deleteGuestButtonHandler(elem.id)}}>Удалить</button>
               </li>
             )
           })}
